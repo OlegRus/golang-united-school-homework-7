7d63b4e038b28f16b90b6ce081d766cba9302f83
olegRus <oleg.rus178@gmail.com>
2022-06-18 20:39:54 +0300

    tests for peaople's methods

--- students_test.go
+++ students_test.go
@@ -2,6 +2,8 @@ package coverage
 
 import (
 	"os"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -17,4 +19,150 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
+var (
+	P1 = Person {
+		firstName: "a",
+		lastName: "a",
+		birthDay: time.Now(),
+	}
+	P2 = Person {
+		firstName: "b",
+		lastName: "b",
+		birthDay: time.Now(),
+	}
+	P3 = Person {
+		firstName: "c",
+		lastName: "c",
+		birthDay: time.Now(),
+	}
+)
 
+func TestSwapPeople(t *testing.T) {
+	people := People {P1, P2, P3}
+	people.Swap(0, 2)
+
+	if (P1 != people[2]) && (P3 != people[0]) && (P2 == people[1]) {
+		t.Errorf("persons didn't swap")
+	}
+}
+
+func TestSwapPeopleWithSameIndex(t *testing.T) {
+	people := People {P1, P2, P3}
+	people.Swap(0, 0)
+
+	if !((P1 == people[0]) && (P2 == people[1]) && (P3 == people[2])) {
+		t.Errorf("persons should not to swap")
+	}
+}
+
+func TestSwapPeopleIndexOutOfRange(t *testing.T) {
+	people := People {P1, P2, P3}
+	people.Swap(0, 3)
+
+	if !((P1 == people[0]) && (P2 == people[1]) && (P3 == people[2])) {
+		t.Errorf("persons should not to swap")
+	}
+}
+
+func TestPeopleLen(t *testing.T) {
+	t.Parallel()
+	table := map[string]struct {
+		Persons People
+		Expected int
+	} {
+		"empty": {People{}, 0},
+		"single person": {People{P1}, 1},
+		"many persons": {People{P1, P2, P3}, 3},
+	}
+	for name, tcase := range table {
+		t.Run(name, func(t *testing.T) {
+			if tcase.Persons.Len() != tcase.Expected {
+				t.Errorf("Wrong len result for People")
+			}
+		})
+	}
+}
+
+func TestPeopleLess(t *testing.T) {
+	t.Parallel()
+
+	date := time.Now()
+	firstName := "a"
+	lastName := "a"
+
+	table := map[string]struct {
+		Persons People
+	} {
+		"same date, same firstName, diff lastName less": {
+			People{
+				Person {
+					firstName: firstName,
+					lastName: lastName + "b",
+					birthDay: date,
+				},
+				Person {
+					firstName: firstName,
+					lastName: lastName + "c",
+					birthDay: date,
+				},
+			},
+		},
+		"same date, diff firstName, diff lastName less": {
+			People{
+				Person {
+					firstName: firstName + "b",
+					lastName: lastName + "c",
+					birthDay: date,
+				},
+				Person {
+					firstName: firstName + "c",
+					lastName: lastName + "b",
+					birthDay: date,
+				},
+			},
+		},
+		"diff date, diff firstName, diff lastName less": {
+			People{
+				Person {
+					firstName: firstName + "c",
+					lastName: lastName + "c",
+					birthDay: date,
+				},
+				Person {
+					firstName: firstName + "b",
+					lastName: lastName + "b",
+					birthDay: date.Add(time.Duration(1)),
+				},
+			},
+		},
+	}
+	for name, tcase := range table {
+		t.Run(name, func (t *testing.T) {
+			if tcase.Persons.Less(0, 1) == false {
+				t.Error("Wrong result of Less function")
+			}
+			if tcase.Persons.Less(1, 0) == true {
+				t.Error("Wrong result of Less function")
+			}
+		})
+	}
+}
+
+func TestPeopleLessIndexOutOfRange(t *testing.T) {
+	date := time.Now()
+	people := People{
+		Person {
+			firstName: "a",
+			lastName: "b" + "b",
+			birthDay: date,
+		},
+		Person {
+			firstName: "a",
+			lastName: "b" + "c",
+			birthDay: date,
+		},
+	}
+	if people.Less(0, 2) == true {
+		t.Errorf("Wrong result of Less function")
+	}  
+}
